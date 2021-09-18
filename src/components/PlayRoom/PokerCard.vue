6ad450247da888051201e2cd7eac5b5f12165762
miladd3 <milad.d3@gmail.com>
2021-09-19 00:13:27 +0200

    PokerCard.vue Component

---
+++ src/components/PlayRoom/PokerCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="poker-card">{{ number }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PokerCard',
+  props: {
+    number: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@use '~@/assets/scss/res/rem.scss';
+@use '~@/assets/scss/res/colors.scss';
+
+.poker-card {
+  width: rem.rem-calc(50px);
+  height: rem.rem-calc(100px);
+  background: colors.$primary-gradient;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: colors.$white;
+  cursor: pointer;
+  border-radius: 4px;
+}
+</style>
