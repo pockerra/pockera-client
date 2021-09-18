084cad1fc3e8de6a281e7bd49ffef03884ca6e3a
miladd3 <milad.d3@gmail.com>
2021-09-19 00:14:33 +0200

    PokerTable.vue component

---
+++ src/components/PlayRoom/PokerTable.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="poker-table">
+    <PokerUserList :users="topUsers" />
+    <div class="table"></div>
+    <PokerUserList :users="bottomUsers" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import PokerUserList from '@/components/PlayRoom/PokerUserList.vue';
+
+export default defineComponent({
+  name: 'PokerTable',
+  components: { PokerUserList },
+  props: {
+    users: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    const usersLength = computed(() => props.users?.length);
+    const halfSize = computed(() => Math.ceil(usersLength.value / 2));
+
+    const topUsers = computed(() => {
+      const users = props.users;
+      if (usersLength.value) return users?.splice(0, halfSize.value);
+      return [];
+    });
+
+    const bottomUsers = computed(() => {
+      const users = props.users;
+      if (usersLength.value && halfSize.value) return users?.splice(-halfSize.value);
+      return [];
+    });
+
+    return {
+      topUsers,
+      bottomUsers,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@use '~@/assets/scss/res/colors.scss';
+
+.poker-table {
+  .table {
+    width: 100%;
+    height: 200px;
+    border-radius: 100px;
+    background-color: colors.$chateau-green;
+  }
+}
+</style>
