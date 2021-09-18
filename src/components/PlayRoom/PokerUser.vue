3f03e95b1d201d0d02b4dff301fbb4458b5ae693
miladd3 <milad.d3@gmail.com>
2021-09-19 00:14:04 +0200

    PokerUser.vue and PokerUserList.vue component

---
+++ src/components/PlayRoom/PokerUser.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="poker-user">
+    <slot>{{ name }}</slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PokerUser',
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@use '~@/assets/scss/res/colors.scss';
+.poker-user {
+  background: colors.$alto-gray;
+  border-radius: 10px;
+  width: 6rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/PlayRoom/PokerUserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="poker-user-list" v-if="users">
+    <PokerUser v-for="user in users" :key="user">{{ user }}</PokerUser>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PokerUser from '@/components/PlayRoom/PokerUser.vue';
+
+export default defineComponent({
+  name: 'PokerUserList',
+  components: { PokerUser },
+  props: {
+    users: {
+      type: Array,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.poker-user-list {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 0;
+}
+</style>
