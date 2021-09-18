9e91c223a12da8c8653d20ae9dfdcbe560cef513
miladd3 <milad.d3@gmail.com>
2021-09-19 00:13:36 +0200

    PokerCardList.vue component

---
+++ src/components/PlayRoom/PokerCardList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="poker-card-list">
+    <PokerCard v-for="card in fibonacci" :key="card" :number="card" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import fibonacci from '@/libs/fibonacci';
+import PokerCard from '@/components/PlayRoom/PokerCard.vue';
+
+export default defineComponent({
+  name: 'PokerCardList',
+  components: { PokerCard },
+  setup() {
+    return { fibonacci };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.poker-card-list {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
